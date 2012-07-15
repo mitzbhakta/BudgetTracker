8aa076545135245e0755117d9119d6f0a6f440e7
Mitz <mitzbhakta@gmail.com>
2012-07-15 20:43:11 +0100

    Initial setup test

---
+++ BudgetTracker/budgettracker.cpp
@@ -0,0 +1,12 @@
+#include "budgettracker.h"
+
+BudgetTracker::BudgetTracker(QWidget *parent, Qt::WFlags flags)
+	: QMainWindow(parent, flags)
+{
+	ui.setupUi(this);
+}
+
+BudgetTracker::~BudgetTracker()
+{
+
+}
