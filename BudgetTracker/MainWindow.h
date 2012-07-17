12ebce489e94450c95b93abefe8ec6e22464d1e6
Mitz <mitzbhakta@gmail.com>
2012-07-17 13:48:57 +0100

    Test commit

--- BudgetTracker/MainWindow.h
+++ BudgetTracker/MainWindow.h
@@ -34,6 +34,8 @@ namespace BudgetTracker {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	protected: 
 
 	private:
 		/// <summary>
@@ -48,13 +50,24 @@ namespace BudgetTracker {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			this->button1 = (gcnew System::Windows::Forms::Button());
 			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(197, 227);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
 			// 
 			// MainWindow
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->ClientSize = System::Drawing::Size(284, 262);
+			this->Controls->Add(this->button1);
 			this->Name = L"MainWindow";
 			this->Text = L"MainWindow";
 			this->ResumeLayout(false);
